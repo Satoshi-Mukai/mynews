fdbd0c43bc6516478febe900a5ad43c85b7750e0
satoshi <satoshi@mukai.win>
2025-01-03 12:23:44 +0000

    課題10 adminのprofile作成・編集のViewを作成

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>プロフィール新規作成ーMyNews</title>
+  </head>
+<body>
+  <h1>Myニュース-プロフィール新規作成画面</h1>
+</body>
+</html>
---
+++ resources/views/admin/profile/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <title>プロフィール編集ーMyNews</title>
+  </head>
+<body>
+  <h1>Myニュース-プロフィール編集画面</h1>
+</body>
+</html>
