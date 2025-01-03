53a46d759aaa19fe84f1ad050414c2bd9a8a162b
satoshi <satoshi@mukai.win>
2025-01-03 11:31:28 +0000

    09課題3/4のRouting設定を追加

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*  http://XXXXXX.jp/admin/news/create にアクセスが来たら、
+    Controller Admin\NewsController のAction addに渡す という設定   */
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function(){
+    Route::get('news/create', 'add');
+});
+
+
+
+/* 課題3（PHP/Laravel 09 Routingについて理解する）
+「http://XXXXXX.jp/XXX というアクセスが来たときに、
+ AAAControllerのbbbというAction に渡すRoutingの設定」を書いてみてください　*/
+
+use App\Http\Controllers\AAAController;
+Route::controller(AAAController::class)->group(function(){
+    Route::get('XXX', 'bbb');
+});
+
+
+//課題4（PHP/Laravel 09 Routingについて理解する）
+//admin/profile/create にアクセスしたら ProfileController の add Action に、
+//admin/profile/edit にアクセスしたら ProfileController の edit Action に割り当てるように設定
+
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->prefix('admin')->group(function(){
+    Route::get('profile/create', 'add');
+    Route::get('profile/edit', 'edit');
+});
