b763f04cfe026f7be6ab9f90421d0b6fb3fcbf1e
satoshi <satoshi@mukai.win>
2025-01-03 13:37:29 +0000

    課題11で作成・修正したファイルをアップ

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,12 +1,16 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
-    <title>プロフィール新規作成ーMyNews</title>
-  </head>
-<body>
-  <h1>Myニュース-プロフィール新規作成画面</h1>
-</body>
-</html>
+{{-- layouts/admin.blade.phpを読み込む　--}}
+@extends('layouts.admin')
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>プロフィール新規作成</h2>
+            </div>
+        </div>
+    </div>
+@endsection
