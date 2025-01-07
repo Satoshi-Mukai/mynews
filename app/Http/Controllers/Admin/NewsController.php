31905465efd42d552906899997df36017d8e8380
satoshi <satoshi@mukai.win>
2025-01-07 11:35:41 +0000

    No.23課題作成前状態

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,4 +12,11 @@ class NewsController extends Controller
     {
         return view('admin.news.create');
     }
+
+    // 以下を追記（2025/01/07）
+    public function create(Request $request)
+    {
+        //admin/news/createにリダイレクトする
+        return rediredt('admin/news/create');
+    }
 }
